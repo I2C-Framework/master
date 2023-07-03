5878ab0535aa28db6738b38711926c1ccc8602c7
KevinJordil <kevin.jordil@outlook.com>
2023-07-03 17:37:13 +0200

    Adapt size

--- i2c_update_firmware.py
+++ i2c_update_firmware.py
@@ -2,10 +2,19 @@ from smbus2 import SMBus, i2c_msg
 import os
 import math
 import time
+import sys
 
-SLAVE_ADDRESS = 0x42
+slave_address = 0x13
 FIRMWARE_REGISTER = 0x10
-FIRMWARE_PATH = "./store-data.bin"
+FIRMWARE_PATH = "./store-data_alone.bin"
+
+# Get slave address as application argument
+slave_address = 0x00
+if(len(sys.argv) != 2):
+    print("Error: slave address not provided")
+    sys.exit()
+else:
+    slave_address = int(sys.argv[1], 16)
 
 with SMBus(1) as bus:
     
@@ -20,18 +29,21 @@ with SMBus(1) as bus:
     if(len(firmware_content) != file_size):
         print("Error: file size does not match")
            
+    msg = i2c_msg.write(slave_address, [FIRMWARE_REGISTER])
+    bus.i2c_rdwr(msg)
+    time.sleep(1)
 
-    # Send file as part of 1024 byte chunks
-    for i in range(0, file_size, 1024):
+    # Send file as part of 2048 byte chunks
+    for i in range(0, file_size, 2048):
         chunk = []
-        if(i + 1024 > file_size):
+        if(i + 2048 > file_size):
             chunk = firmware_content[i:]
         else:
-            chunk = firmware_content[i:i+1024]
+            chunk = firmware_content[i:i+2048]
         # Print chunck number
-        print("Chunk number: " + str(i//1024+1) + " of " + str(math.ceil(file_size/1024)))
+        print("Chunk number: " + str(i//2048+1) + " of " + str(math.ceil(file_size/2048)))
         # Message format: [register, block number, number of blocks in file, data]
-        msg = i2c_msg.write(SLAVE_ADDRESS, [FIRMWARE_REGISTER, i//1024+1, math.ceil(file_size/1024)] + list(chunk))
+        msg = i2c_msg.write(slave_address, [i//2048+1, math.ceil(file_size/2048)] + list(chunk))
         bus.i2c_rdwr(msg)
-        # Wait for 100ms
+        # Wait for 10ms
         time.sleep(0.01)
