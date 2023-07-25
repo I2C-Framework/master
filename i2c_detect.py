40d6317987a6f012a5bb6351712eb329e5718c0b
KevinJordil <kevin.jordil@outlook.com>
2023-07-25 19:02:33 +0200

    Adapt for framework

--- i2c_detect.py
+++ i2c_detect.py
@@ -1,51 +1,71 @@
+import click
 from smbus2 import SMBus, i2c_msg
 import time
 
-ID_REGISTER = 0x1
-MAJOR_VERSION_REGISTER = 0x2
-MINOR_VERSION_REGISTER = 0x3
-FIX_VERSION_REGISTER = 0x4
+MAGIC_NUMBER = 0x42
+RANGE_MIN = 0x10
+RANGE_MAX = 0x78
 
+ID_REGISTER = 0xA1
+MAJOR_VERSION_REGISTER = 0xA2
+MINOR_VERSION_REGISTER = 0xA3
+FIX_VERSION_REGISTER = 0xA4
+GROUP_REGISTER = 0xA5
+SENSORS_TYPE_REGISTER = 0xA6
+NAME_REGISTER = 0xA7
 
+def get_string_register(bus, address, register, length):
+    write = i2c_msg.write(address, [register])
+    read = i2c_msg.read(address, length)
+    bus.i2c_rdwr(write, read)
+    bytes_data = list(read)
+    response_text = "".join(chr(byte) for byte in bytes_data)
+    return response_text
+    
 
-# Function to send message vy i2c
-def send_message(bus, address, register, length):
-    # Write ID register
-    msg = i2c_msg.write(address, [register])
-    # Send message
-    bus.i2c_rdwr(msg)
-    # Read ID register
-    msg = i2c_msg.read(address, length)
-    # Send message
-    bus.i2c_rdwr(msg)
+# Function to send message i2c
+def get_int_register(bus, address, register, length):
+    write = i2c_msg.write(address, [register])
+    read = i2c_msg.read(address, length)
+    bus.i2c_rdwr(write, read)
     data = 0
     for i in range(0, length):
-        data += int.from_bytes(msg.buf[i], byteorder='big') << (8*i)
+        data += int.from_bytes(read.buf[i], byteorder='big') << (8*i)
     return data
 
-    
-bus = SMBus(1)    
+@click.command()
+@click.option('-b', '--bus', 'bus_number', default=1, prompt='I2C bus number', help='The I2C bus number to be used.', type=int)
 
-print("#  uuid        i2c      firmware")
-print("--------------------------------")
-counter = 0
+def detect(bus_number):
+    """ Program to detect all I2C devices belonging to the I2C ecosystem """    
+    bus = SMBus(bus_number)    
 
-# Detect all devices
-for i in range(0x10, 0x78):
-    # Write ID register
-    msg = i2c_msg.read(i, 1)
-    # Send message
-    try:
-        bus.i2c_rdwr(msg)
-    except:
-        pass
-    # Check if response is 0x97
-    if(int.from_bytes(msg.buf[0], byteorder='big') == 0x97):
-        id = send_message(bus, i, ID_REGISTER, 4)
-        major_version = send_message(bus, i, MAJOR_VERSION_REGISTER, 2)
-        minor_version = send_message(bus, i, MINOR_VERSION_REGISTER, 2)
-        fix_version = send_message(bus, i, FIX_VERSION_REGISTER, 2)
-        
-        counter += 1
-        
-        print(str(counter) + ". " + hex(id) + "    " + hex(i) + "     " + str(major_version) + "." + str(minor_version) + "." + str(fix_version))
+    print("#  uuid        i2c      firmware   group  type            name")
+    print("----------------------------------------------------------------")
+    counter = 0
+
+    # Detect all devices
+    for i in range(RANGE_MIN, RANGE_MAX):
+        # Write ID register
+        msg = i2c_msg.read(i, 1)
+        # Send message
+        try:
+            bus.i2c_rdwr(msg)
+        except:
+            continue
+
+        if(int.from_bytes(msg.buf[0], byteorder='little') == MAGIC_NUMBER):
+            id = get_int_register(bus, i, ID_REGISTER, 4)
+            major_version = get_int_register(bus, i, MAJOR_VERSION_REGISTER, 1)
+            minor_version = get_int_register(bus, i, MINOR_VERSION_REGISTER, 1)
+            fix_version = get_int_register(bus, i, FIX_VERSION_REGISTER, 1)
+            group = get_int_register(bus, i, GROUP_REGISTER, 1)
+            sensors_type = get_string_register(bus, i, SENSORS_TYPE_REGISTER, 32)
+            name = get_string_register(bus, i, NAME_REGISTER, 32)
+            
+            counter += 1
+            
+            print(str(counter) + ". " + hex(id) + "    " + hex(i) + "     " + str(major_version) + "." + str(minor_version) + "." + str(fix_version) + "      " + str(group) + "      " + sensors_type + "    " + name)
+
+if __name__ == '__main__':
+    detect()
--- i2c_update_firmware.py
+++ i2c_update_firmware.py
@@ -1,49 +1,126 @@
-from smbus2 import SMBus, i2c_msg
+import click
 import os
 import math
 import time
-import sys
+from smbus2 import SMBus, i2c_msg
 
-slave_address = 0x13
-FIRMWARE_REGISTER = 0x10
-FIRMWARE_PATH = "./store-data_alone.bin"
+MAGIC_NUMBER = 0x42
+FIRMWARE_REGISTER = 0xA0
+RANGE_MIN = 0x10
+RANGE_MAX = 0x78
+GROUP_REGISTER = 0xA5
+SENSORS_TYPE_REGISTER = 0xA6
 
-# Get slave address as application argument
-slave_address = 0x00
-if(len(sys.argv) != 2):
-    print("Error: slave address not provided")
-    sys.exit()
-else:
-    slave_address = int(sys.argv[1], 16)
-
-with SMBus(1) as bus:
-    
-    file_size = os.path.getsize(FIRMWARE_PATH)
-    
-    # open firmware file
-    firmware = open(FIRMWARE_PATH, "rb")
-    
-    firmware_content = bytearray(firmware.read())
-    #print("Firmware content: " + str(list(firmware_content)))
-    
-    if(len(firmware_content) != file_size):
-        print("Error: file size does not match")
-           
-    msg = i2c_msg.write(slave_address, [FIRMWARE_REGISTER])
+# Function to send message vy i2c
+def send_message(bus, address, register, length):
+    msg = i2c_msg.write(address, [register])
     bus.i2c_rdwr(msg)
-    time.sleep(1)
+    msg = i2c_msg.read(address, length)
+    bus.i2c_rdwr(msg)
+    data = 0
+    #for i, b in enumerate(msg.buf):
+    for i in range(0, length):
+        data += int.from_bytes(msg.buf[i], byteorder='big') << (8*i)
+    return data
 
-    # Send file as part of 2048 byte chunks
-    for i in range(0, file_size, 2048):
+def validate_hex(ctx, param, value):
+    try:
+        address = int(value[2:], 16)
+        return address
+    except (ValueError):
+        raise click.BadParameter(f'{value} is not a valid hexadecimal number.')
+    except (TypeError):
+        return None
+    
+def send_firmware(bus, slave_address, firmware_content, file_size, restart_firmware):
+    if(restart_firmware):
+        msg = i2c_msg.write(slave_address, [FIRMWARE_REGISTER])
+        bus.i2c_rdwr(msg)
+        time.sleep(2)
+
+    # Send file as part of 1024 byte chunks
+    for i in range(0, file_size, 1024):
         chunk = []
-        if(i + 2048 > file_size):
+        if i + 1024 > file_size:
             chunk = firmware_content[i:]
         else:
-            chunk = firmware_content[i:i+2048]
-        # Print chunck number
-        print("Chunk number: " + str(i//2048+1) + " of " + str(math.ceil(file_size/2048)))
+            chunk = firmware_content[i:i+1024]
+
+        click.echo("Chunk number: " + str(i//1024+1) + " of " + str(math.ceil(file_size/1024)))
         # Message format: [register, block number, number of blocks in file, data]
-        msg = i2c_msg.write(slave_address, [i//2048+1, math.ceil(file_size/2048)] + list(chunk))
+        msg = i2c_msg.write(slave_address, [i//1024+1, math.ceil(file_size/1024)] + list(chunk))
         bus.i2c_rdwr(msg)
-        # Wait for 10ms
-        time.sleep(0.01)
+        
+    click.echo("Firmware sent successfully")
+
+
+@click.command()
+@click.option('-b', '--bus', 'bus_number', default=1, prompt='I2C bus number',
+              help='The I2C bus number to be used.', type=int)
+@click.option('-f', '--firmware', 'firmware_path', prompt='Firmware path',
+              help='The path to the firmware file to be sent.', type=str)
+@click.option('-a', '--address', 'slave_address',
+              help='The address of the I2C slave to be updated as hex (0x--).', callback=validate_hex, type=str)
+@click.option('-r', '--restart', 'restart_firmware',
+              help='Restart the firmware to go into bootloader mode for update.', is_flag=True)
+@click.option('-g', '--group', 'device_group',
+              help='The group of the device to be updated.', type=int)
+@click.option('-s', '--sensors', 'sensors_type',
+              help='The type of sensors of the device to be updated.', type=int)
+
+def update_firmware(bus_number, firmware_path, slave_address, device_group, sensors_type, restart_firmware):
+    """ Program to update the firmware of a microcontroller belonging to the I2C ecosystem """  
+    count = sum([1 for arg in [slave_address, device_group, sensors_type] if arg is not None])
+    if count != 1:
+        raise click.UsageError('One and only one of the following arguments must be provided: --address, --group, --sensors')
+
+    with SMBus(bus_number) as bus:
+        file_size = os.path.getsize(firmware_path)
+        firmware = open(firmware_path, "rb")
+        firmware_content = bytearray(firmware.read())
+
+        if len(firmware_content) != file_size:
+            click.echo("Error: file size does not match")
+            return
+        
+        if slave_address is not None:
+            send_firmware(bus, slave_address, firmware_content, file_size, restart_firmware)
+        elif device_group is not None:
+            # Detect all devices
+            for i in range(RANGE_MIN, RANGE_MAX):
+                # Write ID register
+                msg = i2c_msg.read(i, 1)
+                # Send message
+                try:
+                    bus.i2c_rdwr(msg)
+                except:
+                    continue
+
+                if(int.from_bytes(msg.buf[0], byteorder='little') == MAGIC_NUMBER):
+                    group = send_message(bus, i, GROUP_REGISTER, 1)
+                    if(group == device_group):
+                        # Send firmware
+                        click.echo("Updating device with address: " + hex(i))
+                        send_firmware(bus, i, firmware_content, file_size, restart_firmware)
+        elif sensors_type is not None:
+            # Detect all devices
+            for i in range(RANGE_MIN, RANGE_MAX):
+                # Write ID register
+                msg = i2c_msg.read(i, 1)
+                # Send message
+                try:
+                    bus.i2c_rdwr(msg)
+                except:
+                    continue
+
+                if(int.from_bytes(msg.buf[0], byteorder='little') == MAGIC_NUMBER):
+                    group = send_message(bus, i, SENSORS_TYPE_REGISTER, 1)
+                    if(group == device_group):
+                        # Send firmware
+                        click.echo("Updating device with address: " + hex(i))
+                        send_firmware(bus, i, firmware_content, file_size, restart_firmware) 
+            
+        
+
+if __name__ == '__main__':
+    update_firmware()
