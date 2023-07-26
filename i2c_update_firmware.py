daabc19337fd8f41a3c38aa4476b10b04cb341fe
KevinJordil <kevin.jordil@outlook.com>
2023-07-26 17:57:52 +0200

    Improve user experience

--- i2c_update_firmware.py
+++ i2c_update_firmware.py
@@ -11,16 +11,23 @@ RANGE_MAX = 0x78
 GROUP_REGISTER = 0xA3
 SENSORS_TYPE_REGISTER = 0xA4
 
-# Function to send message vy i2c
-def send_message(bus, address, register, length):
-    msg = i2c_msg.write(address, [register])
-    bus.i2c_rdwr(msg)
-    msg = i2c_msg.read(address, length)
-    bus.i2c_rdwr(msg)
+# Function to get a register value as a string
+def get_string_register(bus, address, register, length):
+    write = i2c_msg.write(address, [register])
+    read = i2c_msg.read(address, length)
+    bus.i2c_rdwr(write, read)
+    bytes_data = list(read)
+    response_text = "".join(chr(byte) for byte in bytes_data)
+    return response_text
+
+# Function to get a register value as an integer
+def get_int_register(bus, address, register, length):
+    write = i2c_msg.write(address, [register])
+    read = i2c_msg.read(address, length)
+    bus.i2c_rdwr(write, read)
     data = 0
-    #for i, b in enumerate(msg.buf):
     for i in range(0, length):
-        data += int.from_bytes(msg.buf[i], byteorder='big') << (8*i)
+        data += int.from_bytes(read.buf[i], byteorder='big') << (8*i)
     return data
 
 def validate_hex(ctx, param, value):
@@ -34,22 +41,23 @@ def validate_hex(ctx, param, value):
     
 def send_firmware(bus, slave_address, firmware_content, file_size, restart_firmware):
     if(restart_firmware):
+        print("Restarting firmware")
         msg = i2c_msg.write(slave_address, [FIRMWARE_REGISTER])
         bus.i2c_rdwr(msg)
         time.sleep(2)
 
     # Send file as part of 1024 byte chunks
-    for i in range(0, file_size, 1024):
-        chunk = []
-        if i + 1024 > file_size:
-            chunk = firmware_content[i:]
-        else:
-            chunk = firmware_content[i:i+1024]
+    with click.progressbar(range(0, file_size, 1024), label="Upload firmware" ) as bar:
+        for i in bar:
+            chunk = []
+            if i + 1024 > file_size:
+                chunk = firmware_content[i:]
+            else:
+                chunk = firmware_content[i:i+1024]
 
-        click.echo("Chunk number: " + str(i//1024+1) + " of " + str(math.ceil(file_size/1024)))
-        # Message format: [register, block number, number of blocks in file, data]
-        msg = i2c_msg.write(slave_address, [i//1024+1, math.ceil(file_size/1024)] + list(chunk))
-        bus.i2c_rdwr(msg)
+            # Message format: [block number, number of blocks in file, data]
+            msg = i2c_msg.write(slave_address, [i//1024+1, math.ceil(file_size/1024)] + list(chunk))
+            bus.i2c_rdwr(msg)
         
     click.echo("Firmware sent successfully")
 
@@ -65,12 +73,12 @@ def send_firmware(bus, slave_address, firmware_content, file_size, restart_firmw
               help='Restart the firmware to go into bootloader mode for update.', is_flag=True)
 @click.option('-g', '--group', 'device_group',
               help='The group of the device to be updated.', type=int)
-@click.option('-s', '--sensors', 'sensors_type',
-              help='The type of sensors of the device to be updated.', type=int)
+@click.option('-t', '--type', 'device_sensors_type',
+              help='The type of sensors of the device to be updated.', type=str)
 
-def update_firmware(bus_number, firmware_path, slave_address, device_group, sensors_type, restart_firmware):
+def update_firmware(bus_number, firmware_path, slave_address, device_group, device_sensors_type, restart_firmware):
     """ Program to update the firmware of a microcontroller belonging to the I2C ecosystem """  
-    count = sum([1 for arg in [slave_address, device_group, sensors_type] if arg is not None])
+    count = sum([1 for arg in [slave_address, device_group, device_sensors_type] if arg is not None])
     if count != 1:
         raise click.UsageError('One and only one of the following arguments must be provided: --address, --group, --sensors')
 
@@ -97,12 +105,15 @@ def update_firmware(bus_number, firmware_path, slave_address, device_group, sens
                     continue
 
                 if(int.from_bytes(msg.buf[0], byteorder='little') == MAGIC_NUMBER):
-                    group = send_message(bus, i, GROUP_REGISTER, 1)
+                    group = get_int_register(bus, i, GROUP_REGISTER, 1)
                     if(group == device_group):
                         # Send firmware
-                        click.echo("Updating device with address: " + hex(i))
+                        click.echo("-" * 62)
+                        click.echo("Device address: " + hex(i))
                         send_firmware(bus, i, firmware_content, file_size, restart_firmware)
-        elif sensors_type is not None:
+            click.echo("-" * 62)    
+            
+        elif device_sensors_type is not None:
             # Detect all devices
             for i in range(RANGE_MIN, RANGE_MAX):
                 # Write ID register
@@ -114,11 +125,15 @@ def update_firmware(bus_number, firmware_path, slave_address, device_group, sens
                     continue
 
                 if(int.from_bytes(msg.buf[0], byteorder='little') == MAGIC_NUMBER):
-                    group = send_message(bus, i, SENSORS_TYPE_REGISTER, 1)
-                    if(group == device_group):
+                    sensors_type = get_string_register(bus, i, SENSORS_TYPE_REGISTER, 32)
+                    # Remove \x00 characters from string
+                    sensors_type = sensors_type.replace('\x00', '')
+                    if(sensors_type == device_sensors_type):
                         # Send firmware
-                        click.echo("Updating device with address: " + hex(i))
+                        click.echo("-" * 62)
+                        click.echo("Device address: " + hex(i))
                         send_firmware(bus, i, firmware_content, file_size, restart_firmware) 
+            click.echo("-" * 62)
 
 if __name__ == '__main__':
     update_firmware()
